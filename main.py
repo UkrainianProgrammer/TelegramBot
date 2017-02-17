7250e78178d1671567ec711b5b2e8645a792101d
Oleksandr Sofishchenko <insten490@gmail.com>
2017-02-16 20:44:29 -0600

    ready to parse json of random comic

--- main.py
+++ main.py
@@ -3,9 +3,10 @@
 
 import telebot
 import config
-import urllib2 as urllib
+#import urllib2 as urllib
 from cStringIO import StringIO
 from PIL import Image
+import urllib, json
 
 bot = telebot.TeleBot(config.token)
 
@@ -41,17 +42,21 @@ comic {id} - fetch id specific comic
 additional commands may apply (/start, /help etc.)
 """
 
+
 def fetch_random():
     #generate random number
     #construct url based on random id
     #fetch image using xkcd json format
+    #need to parse json to extract image url
     from random import randint
     id = randint(1, 2000)
+    print ("id is" + str(id) + "\n")
     url = "http://xkcd.com/" + str(id) + "/info.0.json"
-    img_file = urllib.urlopen(url)
-    im = StringIO(img_file.read())
-    img = Image.open(im)
-    return img
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+    print data
+
+    #return None
 
 
 
@@ -59,24 +64,13 @@ def fetch_random():
 def handle_text(message):
     #answer = "sorry, haven't learnt that yet."
     if message.text == "random comic":
-        print("test message")
-        comic = fetch_random()
-        bot.send_message(message.chat.id, comic)
+        print("test message\n")
+        fetch_random()
+        #bot.send_message(message.chat.id, comic)
         #answer = "Hi there"
         #log(message, answer)
         #bot.send_message(message.chat.id, answer)
-    """
-    elif message.text== "not a test":
-        answer = "I know that too."
-        log(message, answer)
-        bot.send_message(message.chat.id, answer)
-    elif message.text == "1" or message.text == "2":
-        bot.send_message(message.chat.id, "I received either 1 or 2")
-    elif message.text == "bot, reply" and str(message.from_user.id) == "244646820":
-        bot.send_message(message.chat.id, "id match - yes \nwelcome!")
     else:
-        bot.send_message(message.chat.id, answer)
-        log(message, answer)
-    """
+        print ("Hello\n")
 
 bot.polling()
