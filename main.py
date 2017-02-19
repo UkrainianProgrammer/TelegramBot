4545230ddb9eef84bf4a7823352029dcd63a05ac
Oleksandr Sofishchenko <insten490@gmail.com>
2017-02-19 01:27:12 -0600

    picture now shows up inside the app; made commands case insensitive

--- main.py
+++ main.py
@@ -1,79 +1,126 @@
 # Initial version of my script for updater bot
 # must be compiled using Python 3.6.x
 
+# a few lines of code were borrowed from
+# https://github.com/1995eaton/xkcd_downloader/blob/master/xkcd_downloader.py
+
+
 import telebot
 import config
-#import urllib2 as urllib
-#from StringIO import StringIO
-from PIL import Image
-#import json
 import requests
-from bs4 import BeautifulSoup
-from io import BytesIO
-#import urllib.request as urllib2
+#from bs4 import BeautifulSoup
+from re import search
+import os
+
 
 bot = telebot.TeleBot(config.token)
 
+# for testing only
 print (bot.get_me())
 
 """
-will have 2 command that work as text messages:
+have two commands that work as text messages:
 random comic - fetch random
 comic {id} - fetch id specific comic
 
-additional commands may apply (/start, /help etc.)
+additional commands apply (/start, /help)
 """
 
 """
- idea: TODO: add comic info to the app screen (title, text etc.)
+ idea: fetch comic by name (?)
  TODO: finish main commands and write help documentation using BotFather
+ TODO: add custom keyboard (!)
 """
 
-def fetch_random(message):
-    from random import randint
-    id = randint(1, 1800)
-    return generate_url(message, id)
+#def fetch_random(message):
+    #from random import randint
+    #id = randint(1, 1800)
+    #return display_image(message, id)
 
-def fetch_by_id(message, id):
-    return generate_url(message, id)
-
-# routine below will be called in the fetch_... blocks above
-def generate_url(message, id):
-    url = "http://xkcd.com/" + str(id) + "/info.0.json"
-    req = requests.get(url)
-    json_data = req.json()
-    img_url = json_data["img"]
-    comic_info = "Title: {0}\nText: {1}".format(json_data["title"], json_data["transcript"])
-    bot.send_message(message.from_user.id, comic_info)
-    print(img_url)
-    return img_url
+#def fetch_by_id(message, id):
+    #return display_image(message, id)
 
 # display random or id-specific comic routine called in handle_text
-def display_image(message, img_url):
-    cont = requests.get(img_url)
-    img = Image.open(BytesIO(cont.content)).show()
-    bot.send_chat_action(message.from_user.id, 'upload_photo')
+def display_image(message, id):
+    # construct json url
+    url = "http://xkcd.com/" + str(id) + "/info.0.json"
+    req = requests.get(url)
 
-# --------------------------------- #
+    #fetch json file
+    json_data = req.json()
+    img_url = json_data["img"] # corresponding image url string
+
+    # print comic info
+    comic_info = "Title: {0}\nComic ID: {1}\nSummary: {2}".format(json_data["title"], json_data["num"],
+                                                                    json_data["alt"])
+    bot.send_message(message.from_user.id, comic_info)
+    #print(img_url)
+
+    # this is comic image dir
+    directory = "/Users/Alex/Documents/GitHub Folders/TelegramBot/img"
+
+    # the section below is borrowed another GitHub repo (link above)
+    num = str(json_data['num'])
+    image = num + search("\.([a-z])+$", json_data['img']).group()
+
+    with open(directory + '/' + image, 'wb') as image_file:
+        req = requests.get(img_url, stream=True) # bypass ssl verification
+        for block in req.iter_content(1024):
+            if block:
+                image_file.write(block)
+                image_file.flush()
+
+    # open pic and send it to the app
+    img = open(directory + '/' + image, 'rb')
+    bot.send_chat_action(message.from_user.id, "upload_photo")
+    bot.send_photo(message.from_user.id, img)
+    img.close()
+
+    # remove image from server
+    os.chdir(directory)
+    for file in os.listdir(directory):
+        if file.endswith(".jpg") | file.endswith(".png"):
+            os.remove(file)
+
+
+# ----------------------------------------------------------- #
+
 # decorators:
+
+# start and help commands (equivalent for now)
+@bot.message_handler(commands=["start", "help"])
+def handle_text(message):
+    help_string = "Thank you for using xkcd bot for Telegram.\nUse the following commnds:" \
+                  "\nrandom comic - view random comic\n" \
+                  "comic {id} - view id-specific comic"
+    bot.send_message(message.chat.id, help_string)
+
+# all bot commands
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    if message.text == "random comic":
-        img_url = fetch_random(message)
-        display_image(message, img_url)
+    if message.text.lower() == "random comic":
+        from random import randint
+        id = randint(1, 1900)
+        display_image(message, id)
     else:
         # display specific comic
         # check if text starts with word comic
         # find comic id
         # fetch comic
-        if message.text.startswith("comic"):
+        if message.text.lower().startswith("comic"):
             if any(i.isdigit() for i in message.text):
                 id = message.text.split()[1]
-                img_url = fetch_by_id(message, id)
-                display_image(message, img_url)
+                display_image(message, id)
             else:
-                print ("Error: id is missing\n")
+                bot.send_message(message.chat.id, "Please specify id of the comic. For example: comic 45")
+                #print ("Error: id is missing\n")
         else:
-            print ("Error: command is invalid\n")
+            bot.send_message(message.chat.id, "Invalid command, please use /help to learn more.")
+            #print ("Invalid command, please use /help to learn more."\n")
 
+
+# run the bot
 bot.polling(none_stop=True)
+
+#if '__name__' == '__main__':
+    #main()
